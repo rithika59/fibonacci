03af9ce9c328718fc0b79127aa773e93e1a93645
rithika59 <52556920+rithika59@users.noreply.github.com>
2020-06-03 22:39:23 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,24 @@
+/*
+Fibonacci series
+*/
+
+class fibonacci{
+
+    static int n1 =0, n2=1,n3 =0;
+    static void printFibonacci(final int count){
+        if (count > 0) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.println(" " + n3);
+            printFibonacci(count - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        final int count = 10;
+        System.out.println(n1+" "+n2);
+        printFibonacci(count-2);
+    
+    }
+}
